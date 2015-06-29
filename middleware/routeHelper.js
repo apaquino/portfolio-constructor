4b468d3af19b9180d9b7a66db2c25f9d950ae5ab
Armand <apaquino@gmail.com>
2015-06-28 20:10:27 -0700

    add routeHelper method ensureCorrectUserNested for portfolio_id params routes (nested)

--- middleware/routeHelper.js
+++ middleware/routeHelper.js
@@ -13,7 +13,7 @@ var routeHelpers = {
   ensureCorrectUser: function( req, res, next ) {
     db.Portfolio.findById(req.params.id, function(err, portfolio){
       if (portfolio.ownerId !== req.session.id) {
-        res.redirect('/portfolios/' + portfolio.id);
+        res.redirect('/portfolios');
       }
       else {
        return next();
@@ -21,7 +21,18 @@ var routeHelpers = {
     });
   },
 
-  preventLoginSignup: function(req, res, next) {
+  ensureCorrectUserNested: function( req, res, next ) {
+    db.Portfolio.findById(req.params.portfolio_id, function(err, portfolio){
+      if (portfolio.ownerId !== req.session.id) {
+        res.redirect('/portfolios');
+      }
+      else {
+       return next();
+      }
+    });
+  },
+
+  preventLoginSignup: function( req, res, next ) {
     if (req.session.id !== null && req.session.id !== undefined) {
       res.redirect('/portfolios');
     }
