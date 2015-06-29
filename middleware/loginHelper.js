b3bb46662fbc953360d9562e4e441fd4b1265371
Armand <apaquino@gmail.com>
2015-06-29 14:33:46 -0700

    update login helper to store firstname in the session

--- middleware/loginHelper.js
+++ middleware/loginHelper.js
@@ -4,10 +4,12 @@ var loginHelpers = function (req, res, next) {
 
   req.login = function (user) {
     req.session.id = user._id;
+    req.session.firstname= user.firstname;
   };
 
   req.logout = function () {
     req.session.id = null;
+    req.session.firstname = null;
   };
 
   next();
