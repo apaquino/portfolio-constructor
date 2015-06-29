9da04a2d29b85715c9a6a35de618f7b4166ed037
Armand <apaquino@gmail.com>
2015-06-29 16:18:34 -0700

    update app.js for process.env.PORT

--- app.js
+++ app.js
@@ -399,6 +399,4 @@ app.get( '/logout', function ( req, res ) {
   res.render('errors');
 });
 
-app.listen( 3000, function() {
-  console.log('Starting portfolio-constructicon app on port 3000');
-});
+app.listen( process.env.PORT || 3000 );
