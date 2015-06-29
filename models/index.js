55e10562b7d034b55886f435be27246adc33b158
Armand <apaquino@gmail.com>
2015-06-29 16:23:42 -0700

    ready to push to heroku

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 var mongoose = require("mongoose");
-    mongoose.connect("mongodb://localhost/portfolio_app");
+    mongoose.connect( process.env.MONGOLAB_URI || "mongodb://localhost/portfolio_app" );
     mongoose.set( 'debug', true );
 
 module.exports.Portfolio = require( './portfolio' );
