98661df34fbafef98f26a646b861398f00292ad4
Armand <apaquino@gmail.com>
2015-07-01 20:04:50 -0700

    refactor signup form validatio

---
+++ public/js/signup.js
@@ -0,0 +1,57 @@
+$( document ).ready(function() {
+
+  $('.ui.form')
+  .form({
+    fields: {
+      email: {
+        identifier  : 'email',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter your username (email)'
+          },
+          {
+            type: 'email',
+            prompt: 'Please enter a valid email'
+          }
+        ]
+      },
+      pass: {
+        identifier  : 'pass',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter your password'
+          },
+          {
+            type: 'length[6]',
+            prompt: 'Please enter a minimum length of 6'
+          }
+        ]
+      },
+      firstname: {
+        identifier: 'firstname',
+        rules: [
+          {
+            type: 'empty',
+            prompt: 'Please enter your first name'
+          }
+        ]
+      },
+      lastname: {
+        identifier: 'lastname',
+        rules: [
+          {
+            type: 'empty',
+            prompt: 'Please enter your last name'
+          }
+        ]
+      }
+    },
+    on: 'blur',
+    inline: 'true'
+  })
+  ;
+
+})
+;
